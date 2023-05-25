0936d193ed89ab20fb7d8ddfbb71564662546f2a
Kevin Petroni <inuyukibunny@outlook.com>
2023-05-25 15:56:39 -0300

    first commit of this project

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,15 @@
+
+
+const Welcome = () => {
+    return (
+        <div id="Welcome">
+            <h2>Seja bem-vindo </h2>
+            <p>Clique para começar:</p>
+            <button>
+                Iniciar
+            </button>
+        </div>
+    )
+}
+
+export default Welcome
